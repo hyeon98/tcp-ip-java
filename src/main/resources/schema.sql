ac8844c0b312e340462940cb1b6f237399f34f95
hyeon98 <swhyeon98@gmail.com>
2026-06-01 14:03:52 +0900

    fix: config.xml을 찾지 못하는 오류 수정.  - Gradle 표준 경로로 변경

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS products (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(10) NOT NULL,
+    price INT NOT NULL,
+    stock INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
